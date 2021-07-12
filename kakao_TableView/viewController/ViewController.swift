7775e007ba36e7007f50f21952e09e55d87da372
Jiseok97 <wltjr1426@naver.com>
2021-07-12 15:06:00 +0900

    3rd_Start

---
+++ kakao_TableView/viewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kakao_TableView
+//
+//  Created by 이지석 on 2021/07/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
